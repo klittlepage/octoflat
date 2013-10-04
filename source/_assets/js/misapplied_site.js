f2a9550fa5f93782b52a5a21f998711afd25cd6d
Kelly Littlepage <kelly.littlepage@gmail.com>
2013-10-04 15:53:47 -0600

    Initial work...debugging.

---
+++ source/_assets/js/misapplied_site.js
@@ -0,0 +1,8 @@
+//= require libs/jquery.min
+//= require libs/bootstrap.min
+//= require libs/modernizr-2.0
+//= require libs/d3.v3.min
+//= require libs/holder.min
+//= require octopress
+
+!window.jQuery && document.write(unescape('%3Cscript src="{{jquery.min | asset_path}}"%3E%3C/script%3E'))
