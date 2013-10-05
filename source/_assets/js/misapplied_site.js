b5f79349c08ccafd14b78d49ea3dd0dc80e5f614
Kelly Littlepage <kelly.littlepage@gmail.com>
2013-10-05 01:59:18 -0600

    Working pipeline.

--- source/_assets/js/misapplied_site.js
+++ source/_assets/js/misapplied_site.js
@@ -4,5 +4,3 @@
 //= require libs/d3.v3.min
 //= require libs/holder.min
 //= require octopress
-
-!window.jQuery && document.write(unescape('%3Cscript src="{{jquery.min | asset_path}}"%3E%3C/script%3E'))
